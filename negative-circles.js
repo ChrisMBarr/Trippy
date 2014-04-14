f3cebd17a5d790ab06d218f20aa81a19f4f0650c
Chris Barr <chris.m.barr@gmail.com>
2014-04-13 20:22:07 -0500

    Cleanup, optimize & begin on another animation

---
+++ negative-circles.js
@@ -0,0 +1,14 @@
+'strict';
+(function(){
+	var canvas = document.getElementById("draw");
+	var ctx = canvas.getContext("2d");
+	
+	//Set the size
+	var containerSize = 500;
+	canvas.width = containerSize;
+	canvas.height = containerSize;
+	
+
+	
+
+})();
